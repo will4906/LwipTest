1f3999f19309e66cc1766f79a4be98ccd148cfce
will4906 <553105821@qq.com>
2017-01-23 01:46:21 +0800

    初步消除了一直lwip协议栈的报错

--- Object/ethernetif.d
+++ Object/ethernetif.d
@@ -1,2 +1,8 @@
 ..\object\ethernetif.o: drivers\lwip\src\netif\ethernetif.c
 ..\object\ethernetif.o: .\drivers\lwip\src\include\lwip/opt.h
+..\object\ethernetif.o: .\drivers\lwip\src\include\lwipopts.h
+..\object\ethernetif.o: .\drivers\lwip\src\include\lwip/debug.h
+..\object\ethernetif.o: .\drivers\lwip\src\include\lwip/arch.h
+..\object\ethernetif.o: .\drivers\lwip\src\include\lwip/arch/cc.h
+..\object\ethernetif.o: C:\Software\Keil_v5\MDK\ARM\ARMCC\Bin\..\include\stdio.h
+..\object\ethernetif.o: .\drivers\lwip\src\include\lwip/opt.h
---
+++ Object/sys_arch.d
@@ -0,0 +1,12 @@
+..\object\sys_arch.o: drivers\lwip\src\arch\sys_arch.c
+..\object\sys_arch.o: .\drivers\lwip\src\include\lwip/debug.h
+..\object\sys_arch.o: .\drivers\lwip\src\include\lwip/arch.h
+..\object\sys_arch.o: .\drivers\lwip\src\include\lwip/arch/cc.h
+..\object\sys_arch.o: C:\Software\Keil_v5\MDK\ARM\ARMCC\Bin\..\include\stdio.h
+..\object\sys_arch.o: .\drivers\lwip\src\include\lwip/opt.h
+..\object\sys_arch.o: .\drivers\lwip\src\include\lwipopts.h
+..\object\sys_arch.o: .\drivers\lwip\src\include\lwip/debug.h
+..\object\sys_arch.o: .\drivers\lwip\src\include\lwip/def.h
+..\object\sys_arch.o: .\drivers\lwip\src\include\lwip/sys.h
+..\object\sys_arch.o: .\drivers\lwip\src\include\lwip/mem.h
+..\object\sys_arch.o: .\drivers\lwip\src\include\lwip\arch/sys_arch.h
